2e4beecbf0fda5bff9d97e9f48dd1b73594ff2c7
Suraj Gupta <surajrocks1997@gmail.com>
2022-08-13 17:33:43 +0530

    deleting and adding both for specific course and student

---
+++ src/com/demo/hibernate/demo/AddCoursesForMaryDemo.java
@@ -0,0 +1,53 @@
+package com.demo.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.demo.hibernate.demo.entity.Course;
+import com.demo.hibernate.demo.entity.Instructor;
+import com.demo.hibernate.demo.entity.InstructorDetail;
+import com.demo.hibernate.demo.entity.Review;
+import com.demo.hibernate.demo.entity.Student;
+
+public class AddCoursesForMaryDemo {
+
+	public static void main(String[] args) {
+
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class).addAnnotatedClass(Course.class)
+				.addAnnotatedClass(Course.class).addAnnotatedClass(Review.class).addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+
+		Session session = factory.getCurrentSession();
+
+		try {
+			session.beginTransaction();
+
+			int id = 2;
+			Student tempStudent = session.get(Student.class, id);
+			System.out.println("Student Loaded: " + tempStudent);
+			System.out.println("Courses: " + tempStudent.getCourses());
+			
+			Course tempCourse1 = new Course("Rubik's Cube - How to Gain Speed");
+			Course tempCourse2 = new Course("Atari 2600 - Game Development");
+			
+			tempCourse1.addStudent(tempStudent);
+			tempCourse2.addStudent(tempStudent);
+			
+			session.save(tempCourse1);
+			session.save(tempCourse2);
+
+			session.getTransaction().commit();
+
+			System.out.println("Done!");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			session.close();
+			factory.close();
+		}
+	}
+
+}
---
+++ src/com/demo/hibernate/demo/DeletePacmanCourse.java
@@ -0,0 +1,45 @@
+package com.demo.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.demo.hibernate.demo.entity.Course;
+import com.demo.hibernate.demo.entity.Instructor;
+import com.demo.hibernate.demo.entity.InstructorDetail;
+import com.demo.hibernate.demo.entity.Review;
+import com.demo.hibernate.demo.entity.Student;
+
+public class DeletePacmanCourse {
+
+	public static void main(String[] args) {
+
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class).addAnnotatedClass(Course.class)
+				.addAnnotatedClass(Course.class).addAnnotatedClass(Review.class).addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+
+		Session session = factory.getCurrentSession();
+
+		try {
+			session.beginTransaction();
+
+			int id = 11;
+			Course tempCourse = session.get(Course.class, id);
+			
+			System.out.println("Deleting Course: " + tempCourse);
+			session.delete(tempCourse);
+
+			session.getTransaction().commit();
+
+			System.out.println("Done!");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			session.close();
+			factory.close();
+		}
+	}
+
+}
